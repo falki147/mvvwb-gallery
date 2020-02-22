c078a17d59965c6ac382d4b41b276fd0e6b877b9
Florian Preinfalk <preinfalkflorian@gmail.com>
2020-02-22 11:23:05 +0100

    Fix bug where class is redefined

--- src/gallery-mvvwb-gallery.php
+++ src/gallery-mvvwb-gallery.php
@@ -3,81 +3,83 @@
  * Renders the gallery to HTML to the output buffer
  */
 
-/**
- * Wraps the render function in a class
- */
-class MVVWBTemplateGallery {
+if (!class_exists('MVVWBTemplateGallery')) {
     /**
-     * Renders the HTML to the output buffer
-     *
-     * @param object[] $attachments the attached images to the gallery
+     * Wraps the render function in a class
      */
-    public static function buildContent($attachments) {
-        $args = foogallery_gallery_template_arguments();
+    class MVVWBTemplateGallery {
+        /**
+         * Renders the HTML to the output buffer
+         *
+         * @param object[] $attachments the attached images to the gallery
+         */
+        public static function buildContent($attachments) {
+            $args = foogallery_gallery_template_arguments();
 
-        echo '<div class="gallery">';
+            echo '<div class="gallery">';
 
-        $ind = 1;
+            $ind = 1;
 
-        foreach ($attachments as $attachment) {
-            $url = static::getURL($attachment, $args);
+            foreach ($attachments as $attachment) {
+                $url = static::getURL($attachment, $args);
 
-            if ($url !== '')
-                echo '<a href="', esc_url($url), '">';
-            
-            $alt = $attachments->alt;
+                if ($url !== '')
+                    echo '<a href="', esc_url($url), '">';
+                
+                $alt = $attachments->alt;
 
-            if ($alt === '')
-                $alt = "Bild $ind";
+                if ($alt === '')
+                    $alt = "Bild $ind";
 
-            $title = $attachment->caption;
+                $title = $attachment->caption;
 
-            echo '<img src="', foogallery_attachment_html_image_src($attachment, $args), '"';
-            echo ' alt="', esc_attr($alt), '"';
+                echo '<img src="', foogallery_attachment_html_image_src($attachment, $args), '"';
+                echo ' alt="', esc_attr($alt), '"';
 
-            if ($title !== '' && $title !== $alt)
-                echo ' title="', esc_attr($title), '"';
+                if ($title !== '' && $title !== $alt)
+                    echo ' title="', esc_attr($title), '"';
 
-            if (!empty($args['width']) && !empty($args['height']))
-                echo " style=\"width: {$args['width']}px; height: {$args['height']}px\"";
+                if (!empty($args['width']) && !empty($args['height']))
+                    echo " style=\"width: {$args['width']}px; height: {$args['height']}px\"";
 
-            if ($url === $attachment->url) {
-                echo ' data-src="', esc_attr($attachment->url), '"';
-                echo ' data-width="', esc_attr($attachment->width), '"';
-                echo ' data-height="', esc_attr($attachment->height), '"';
+                if ($url === $attachment->url) {
+                    echo ' data-src="', esc_attr($attachment->url), '"';
+                    echo ' data-width="', esc_attr($attachment->width), '"';
+                    echo ' data-height="', esc_attr($attachment->height), '"';
+                }
+
+                echo '>';
+
+                if ($url !== '')
+                    echo '</a>';
+
+                ++$ind;
             }
 
-            echo '>';
-
-            if ($url !== '')
-                echo '</a>';
-
-            ++$ind;
+            echo '</div>';
         }
 
-        echo '</div>';
-    }
+        /**
+         * Gets the URL from an attachment
+         * 
+         * @param object $attachment the attachment object
+         * @param string $args the type of the attachment URL
+         * @return string either the URL or an empty string
+         */
+        public static function getURL($attachment, $args) {
+            switch ($args) {
+            case 'page':
+                return get_attachment_link($foogallery_attachment->ID);
+            case 'custom':
+                if (!isset($args['custom_link']))
+                    return $attachment->custom_url;
 
-    /**
-     * Gets the URL from an attachment
-     * 
-     * @param object $attachment the attachment object
-     * @param string $args the type of the attachment URL
-     * @return string either the URL or an empty string
-     */
-    public static function getURL($attachment, $args) {
-        switch ($args) {
-        case 'page':
-            return get_attachment_link($foogallery_attachment->ID);
-        case 'custom':
-            if (!isset($args['custom_link']))
-                return $attachment->custom_url;
-
-            return $args['custom_link'];
-        case 'none':
-            return '';
-        default:
-            return $attachment->url;
+                return $args['custom_link'];
+            case 'none':
+                return '';
+            default:
+                return $attachment->url;
+            }
         }
     }
 }
